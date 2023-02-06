c5eaf3d3b1650f44d94fedb63205f138677500df
Ramzessoo <117750497+Ramzessoo@users.noreply.github.com>
2023-02-06 18:52:39 +0100

    Wzrorce strukturalne - proxy

---
+++ src/main/java/pattern/structural/proxy/CompanyNetwork.java
@@ -0,0 +1,6 @@
+package pattern.structural.proxy;
+
+public interface CompanyNetwork {
+
+    void obtainAccess(String username);
+}
---
+++ src/main/java/pattern/structural/proxy/ProxyDemoApp.java
@@ -0,0 +1,19 @@
+package pattern.structural.proxy;
+
+import java.util.List;
+
+public class ProxyDemoApp {
+    public static void main(String[] args) {
+
+        var companyEmployees = new CompanyEmployees();
+        List<String> activeEmployees = List.of("Ania", "Marek", "Piotrek", "Michał");
+        companyEmployees.setEmployees(activeEmployees);
+
+        CompanyNetwork network = new ProxyCompanyNetwork(companyEmployees);
+
+        network.obtainAccess("Ania");
+        network.obtainAccess("Monika");
+        network.obtainAccess("Wojtek");
+        network.obtainAccess("Piotrek");
+    }
+}
